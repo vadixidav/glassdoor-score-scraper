72cccf64c2339f2e5196f6bc9ba8b128efa4f42d
Geordon Worley <vadixidav@gmail.com>
2023-01-21 16:41:15 -0600

    add basic tool to find the data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    for finding in scrape(include_str!(
+        "../data/Working-at-Amazon-EI_IE6036.11,17.htm"
+    )) {
+        println!("{finding}");
+        println!();
+        println!();
+    }
+}
+
+fn scrape(page: &str) -> impl Iterator<Item = String> + '_ {
+    page.match_indices("\"employerReviews({")
+        .filter_map(|(start, _)| {
+            page[start..].find("})\"").map(|e| {
+                let end = e + start + 3;
+                page[start..end].to_owned()
+            })
+        })
+        .filter_map(|s| serde_json::from_str::<String>(&s).ok())
 }
